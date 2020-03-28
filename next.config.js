be0a55a3c61a59e184cb04e0cf60b06eec4067db
Sümeyye Özkaynak <s.ozkaynak@outlook.com>
2020-03-28 15:24:30 +0300

    feat(homepage): config file for deployment

--- next.config.js
+++ next.config.js
@@ -1,23 +1,36 @@
-const withOffline = require('next-offline')
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
 
-const config = {
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200
-          }
-        }
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+    }
+  },
+  assetPrefix: '',
+  //assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
       }
-    ]
-  }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }/*,
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // console.log('webpackDevMiddleware');
+    // console.log(config);
+    // Important: return the modified config
+    return config
+  }, */
 }
-
-module.exports = withOffline(config)
