e5438f1bb7b28b530d957d1969472204bf06febd
Sümeyye Özkaynak <s.ozkaynak@outlook.com>
2020-03-28 14:24:15 +0300

    feat(blog): created personel web site.

---
+++ pages/index.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import Typist from 'react-typist'
+
+export default function Index() {
+  const hours = new Date().getHours()
+  const isNoon = hours > 11 && hours < 18
+  const isEvening = (hours >= 0 && hours <= 4) || (hours >= 18 && hours <= 23)
+
+  let welcome = 'Good morning'
+  if (isNoon) welcome = 'Good afternoon'
+  else if (isEvening) welcome = 'Good evening'
+
+  //I build pixel-perfect <span className="nowrap">pages 👩🏻‍💻👌</span>{' '}
+  //smooth <span className="nowrap">animations 🦄</span> and performant
+  //web <span className="nowrap">applications 💯</span>{' '}
+
+  return (
+    <>
+      <div className="hero">
+        <Typist startDelay={1000} cursor={{ element: '▍' }}>
+          Hey,
+          <Typist.Backspace count={4} delay={500} />
+          {welcome},
+          <br />
+          <Typist.Delay ms={300} />
+          <b>
+            <br />
+            <p>
+            As a <span className="nowrap"> full-stack data scientist 👩🏻‍💻 </span>,
+            <br />
+            I enjoy using my obsessive attention to detail,
+            my unequivocal love for making things, and my mission-driven work
+            ethic to literally <span className="nowrap"> change the world 🤲🏼 </span>.
+            <br />
+            That's why I’m excited to make
+            a big impact at a  <span className="nowrap">high growth company 👾 </span>.
+            </p>
+          </b>
+          <br />
+          <Typist.Delay ms={500} />
+          <br />
+          <Typist.Delay ms={500} />I{' '}
+          <a href="https://twitter.com/sumeyyeozkaynak" target="_blank">
+            tweet
+          </a>
+          , take{' '}
+          <a href="https://www.instagram.com/afafet/" target="_blank">
+            photos
+          </a>
+          , push{' '}
+          <a href="https://github.com/sumeyyeozkaynak" target="_blank">
+            code
+          </a>{' '}
+          share{' '}
+          <a href="https://medium.com/@sumeyyeozkaynak" target="_blank">
+            article
+          </a>{' '}
+           and my corporate {' '}
+          <a href="https://www.linkedin.com/in/s%C3%BCmeyye%C3%B6zkaynak/" target="_blank">
+            identity
+          </a>
+          .
+        </Typist>
+      </div>
+
+      <style jsx>{`
+        .hero {
+          padding: 2rem;
+          font-size: 5vw;
+        }
+        @media (min-width: 700px) {
+          .hero {
+            padding: 3rem;
+            font-size: 3vw;
+          }
+        }
+      `}</style>
+      <style global jsx>{`
+        .Cursor {
+          display: inline-block;
+          margin-left: 5px;
+          opacity: 1;
+          animation: blink 1s linear infinite;
+        }
+        @keyframes blink {
+          to {
+            opacity: 0;
+          }
+        }
+      `}</style>
+    </>
+  )
+}
